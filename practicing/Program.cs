5bc35144d7eae8553dc3cf6de31a44824b8d0608
Gabrieroo <53184723+Gabrieroo@users.noreply.github.com>
2020-08-10 19:56:01 -0300

    Add project files.

---
+++ practicing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace practicing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite a medida do primeiro triangulo");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a medida do segundo triangulo");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaY = y.Area();
+            double areaX = x.Area();
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("O triangulo X = " + areaX +" e maior que o triangulo y " + areaY);
+            }
+            else
+            {
+                Console.WriteLine("O triangulo Y = " + areaY + " e maior que o triangulo X " + areaX);
+            }
+
+
+
+        }
+    }
+}
---
+++ practicing/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practicing
+{
+    class Triangulo
+    {
+        public double A,B,C;
+        
+        public double Area()
+        {
+            double index = (A + B + C) / 2.0;
+            return Math.Sqrt(index * (index - A) * (index - B) * (index - C));
+        }
+       
+    }
+}
